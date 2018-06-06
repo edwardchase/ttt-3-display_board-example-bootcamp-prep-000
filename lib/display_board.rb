f69f9c332585ada9fb9cdc59cf80e5a6ae3f39c3
Edward Mixon <edwardchase@gatech.edu>
2018-06-06 18:25:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   row_1 = ['   ','|','   ','|','   '];
+  row_2 = ['   ','|','   ','|','   '];
+  row_3 = ['   ','|','   ','|','   '];
   
 end
 
